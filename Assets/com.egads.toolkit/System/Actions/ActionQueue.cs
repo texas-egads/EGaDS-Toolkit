bdb9eb0ec1f848606e4d0fd53a5fe2a63ec07019
KHodow677 <kylehodowany@utexas.edu>
2023-07-29 20:39:13 -0500

    Comments for Actions System

--- Assets/com.egads.toolkit/System/Actions/ActionQueue.cs
+++ Assets/com.egads.toolkit/System/Actions/ActionQueue.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 
 namespace egads.system.actions
 {
-	/// <summary>
-	/// List of actions that get executed one after another
-	/// </summary>
-	public class ActionQueue
-	{
+    /// <summary>
+    /// Represents a list of actions that get executed one after another (in a queue).
+    /// </summary>
+    public class ActionQueue
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets a value indicating whether the action queue has any content (actions).
+        /// </summary>
+        public bool hasContent => _queue.Count > 0;
+
+        #endregion
+
         #region Private Properties
 
         private Queue<IActionQueueElement> _queue = new Queue<IActionQueueElement>();
@@ -18,76 +27,79 @@ namespace egads.system.actions
         #region Public Methods
 
         /// <summary>
-        /// Should be called every frame by parent object
+        /// Should be called every frame by the parent object to update the action queue.
         /// </summary>
         public void Update()
-		{
-			if (_queue.Count == 0)
-				return;
+        {
+            // If the queue is empty, no need to update anything
+            if (_queue.Count == 0) { return; }
 
-			IActionQueueElement currentElement = _queue.Peek();
-			currentElement.Update();
+            // Get the current action in front of the queue
+            IActionQueueElement currentElement = _queue.Peek();
 
-			if (currentElement.hasEnded)
-			{
-				currentElement.OnExit();
-				_queue.Dequeue();
+            // Update the current action
+            currentElement.Update();
 
-				if (_queue.Count > 0)
-				{
-					currentElement = _queue.Peek();
-					currentElement.OnStart();
-				}
-			}
-		}
+            // Check if the current action has ended
+            if (currentElement.hasEnded)
+            {
+                // Perform OnExit on the current action
+                currentElement.OnExit();
 
-		/// <summary>
-		/// Adds a new element to the back of the queue
-		/// </summary>
-		/// <param name="element"></param>
-		public void Add(IActionQueueElement element)
-		{
-			if (element != null)
-			{
-				_queue.Enqueue(element);
+                // Remove the current action from the queue
+                _queue.Dequeue();
 
-				if (_queue.Count == 1)
-				{
-					element.OnStart();
-				}
-			}
-		}
+                // If there are more actions in the queue, start the next action
+                if (_queue.Count > 0)
+                {
+                    currentElement = _queue.Peek();
+                    currentElement.OnStart();
+                }
+            }
+        }
 
-		/// <summary>
-		/// Adds a time duration where nothing happens
-		/// </summary>
-		/// <param name="duration"></param>
-		public void AddPause(float duration)
-		{
-			Add(new ActionWait(duration));
-		}
+        /// <summary>
+        /// Adds a new element to the back of the action queue.
+        /// </summary>
+        /// <param name="element">The action to add.</param>
+        public void Add(IActionQueueElement element)
+        {
+            if (element != null)
+            {
+                _queue.Enqueue(element);
 
-		/// <summary>
-		/// Calls OnExit on the current element and clears all entries from the queue
-		/// </summary>
-		public void Clear()
-		{
-			if (_queue.Count > 0)
-			{
-				IActionQueueElement currentElement = _queue.Peek();
-				currentElement.OnExit();
+                // If this is the first action in the queue, call its OnStart method.
+                if (_queue.Count == 1) { element.OnStart(); }
+            }
+        }
 
-				_queue.Clear();
-			}
-		}
+        /// <summary>
+        /// Adds a time duration where nothing happens (a pause) to the action queue.
+        /// </summary>
+        /// <param name="duration">The duration of the pause.</param>
+        public void AddPause(float duration)
+        {
+            // Add an action to the queue that waits for the specified duration.
+            Add(new ActionWait(duration));
+        }
 
-		public bool hasContent
-		{
-			get
-			{
-				return _queue.Count > 0;
-			}
-		}
+        /// <summary>
+        /// Calls OnExit on the current element and clears all entries from the action queue.
+        /// </summary>
+        public void Clear()
+        {
+            if (_queue.Count > 0)
+            {
+                // Get the current action in front of the queue
+                IActionQueueElement currentElement = _queue.Peek();
+
+                // Perform OnExit on the current action
+                currentElement.OnExit();
+
+                // Clear the queue
+                _queue.Clear();
+            }
+        }
 
         #endregion
     }
--- Assets/com.egads.toolkit/System/Actions/ActionWait.cs
+++ Assets/com.egads.toolkit/System/Actions/ActionWait.cs
@@ -2,44 +2,61 @@
 
 namespace egads.system.actions
 {
-	public class ActionWait : IActionQueueElement
-	{
+    /// <summary>
+    /// Represents an action that adds a time delay (wait) before completing.
+    /// </summary>
+    public class ActionWait : IActionQueueElement
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets a value indicating whether the wait action has ended.
+        /// </summary>
+        public bool hasEnded => _timer.hasEnded;
+
+        #endregion
+
         #region Private Properties
 
         private Timer _timer;
-
-		private float _duration;
+        private float _duration;
 
         #endregion
 
         #region Public Methods
+
+        /// <summary>
+        /// Creates a new instance of the ActionWait class with the specified duration.
+        /// </summary>
+        /// <param name="duration">The duration of the wait action in seconds.</param>
         public ActionWait(float duration)
-		{
-			_duration = duration;
-		}
+        {
+            _duration = duration;
+        }
 
-		public void OnStart()
-		{
-			_timer = new Timer(_duration);
-		}
+        /// <summary>
+        /// Called when the wait action starts. Initializes the timer with the specified duration.
+        /// </summary>
+        public void OnStart()
+        {
+            _timer = new Timer(_duration);
+        }
 
-		public void Update()
-		{
-			_timer.Update();
-		}
+        /// <summary>
+        /// Updates the wait action by updating the timer.
+        /// </summary>
+        public void Update()
+        {
+            _timer.Update();
+        }
 
-		public void OnExit()
-		{
-
-		}
-
-		public bool hasEnded
-		{
-			get
-			{
-				return _timer.hasEnded;
-			}
-		}
+        /// <summary>
+        /// Called when the wait action is completed or interrupted. It does nothing in this case.
+        /// </summary>
+        public void OnExit()
+        {
+            // Empty: The wait action does not require any cleanup when it completes.
+        }
 
         #endregion
     }
