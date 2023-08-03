b59cebd45e6f4d919a0e4754725ef5fbe3a9ad30
KHodow677 <kylehodowany@utexas.edu>
2023-08-03 11:30:28 +0800

    Added Audio System documentation

--- Assets/com.egads.toolkit/System/Audio/RandomSound.cs
+++ Assets/com.egads.toolkit/System/Audio/RandomSound.cs
@@ -3,14 +3,27 @@ using egads.system.timer;
 
 namespace egads.system.audio
 {
-	public class RandomSound : MonoBehaviour
-	{
+    /// <summary>
+    /// Plays random sounds with configurable pause intervals.
+    /// </summary>
+    public class RandomSound : MonoBehaviour
+    {
         #region Public Properties
 
+        /// <summary>
+        /// The AudioSource used to play the random sounds.
+        /// </summary>
         public AudioSource source;
 
-		public float pauseIntervalMin;
-		public float pauseIntervalMax;
+        /// <summary>
+        /// The minimum interval time between sounds.
+        /// </summary>
+        public float pauseIntervalMin;
+
+        /// <summary>
+        /// The maximum interval time between sounds.
+        /// </summary>
+        public float pauseIntervalMax;
 
         #endregion
 
@@ -23,37 +36,46 @@ namespace egads.system.audio
         #region Unity Methods
 
         private void Awake()
-		{
-			if (source == null) { source = GetComponent<AudioSource>(); }
+        {
+            // If the AudioSource is not set, try to get it from the GameObject
+            if (source == null) { source = GetComponent<AudioSource>(); }
 
-			if (source != null) { source.loop = false; }
+            // Ensure that the AudioSource does not loop the sound
+            if (source != null) { source.loop = false; }
 
-			StartNewTimer();
-		}
+            // Start the timer to play the first sound
+            StartNewTimer();
+        }
 
-		private void Update()
-		{
-			if (source != null && !source.isPlaying)
-			{
-				if (_timer == null) { StartNewTimer(); }
-				else { _timer.Update(); }
+        private void Update()
+        {
+            // Check if the AudioSource is not playing
+            if (source != null && !source.isPlaying)
+            {
+                // If the timer is null, start a new one; otherwise, update the existing timer
+                if (_timer == null) { StartNewTimer(); }
+                else { _timer.Update(); }
 
-				if (_timer.hasEnded)
-				{
-					_timer = null;
-					source.Play();
-				}
-			}
-		}
+                // Check if the timer has ended (time for the next sound)
+                if (_timer.hasEnded)
+                {
+                    _timer = null;
+                    source.Play(); // Play the sound
+                }
+            }
+        }
 
         #endregion
 
         #region Private Methods
 
+        /// <summary>
+        /// Starts a new timer with a random interval between pauseIntervalMin and pauseIntervalMax.
+        /// </summary>
         private void StartNewTimer()
-		{
-			_timer = new Timer(Random.Range(pauseIntervalMin, pauseIntervalMax));
-		}
+        {
+            _timer = new Timer(Random.Range(pauseIntervalMin, pauseIntervalMax));
+        }
 
         #endregion
     }
--- Assets/com.egads.toolkit/System/Audio/SoundCrossFading.cs
+++ Assets/com.egads.toolkit/System/Audio/SoundCrossFading.cs
@@ -4,31 +4,44 @@ using egads.tools.extensions;
 
 namespace egads.system.audio
 {
-	public class SoundCrossFading : MonoBehaviour
-	{
+    /// <summary>
+    /// Crossfades between multiple audio clips for smoother transitions.
+    /// </summary>
+    public class SoundCrossFading : MonoBehaviour
+    {
         #region Constants
 
         const float firstMarker = 0.15f;
-		const float lastMarker = 1.0f - firstMarker;
+        const float lastMarker = 1.0f - firstMarker;
 
         #endregion
 
         #region Public Properties
 
+        /// <summary>
+        /// The list of audio clips to crossfade between.
+        /// </summary>
         public List<AudioClip> clips;
 
-		public AudioSource firstSource;
-		public AudioSource secondSource;
+        /// <summary>
+        /// The first AudioSource used for playback.
+        /// </summary>
+        public AudioSource firstSource;
+
+        /// <summary>
+        /// The second AudioSource used for playback.
+        /// </summary>
+        public AudioSource secondSource;
 
         #endregion
 
         #region Private Properties
 
         private AudioClip _current;
-		private AudioClip _next;
+        private AudioClip _next;
 
-		private AudioSource _currentSource;
-		private AudioSource _nextSource;
+        private AudioSource _currentSource;
+        private AudioSource _nextSource;
 
         private bool _currentHasReachedDeclining = false;
         private bool _choosenNextClip = false;
@@ -36,100 +49,105 @@ namespace egads.system.audio
         #endregion
 
         #region Unity Methods
+
         private void Start()
-		{
-			firstSource.loop = false;
-			secondSource.loop = false;
+        {
+            firstSource.loop = false;
+            secondSource.loop = false;
 
-			_currentSource = firstSource;
-			_nextSource = secondSource;
+            _currentSource = firstSource;
+            _nextSource = secondSource;
 
-			_currentSource.clip = GetNextClip(null);
-			_currentSource.Play();
+            _currentSource.clip = GetNextClip(null);
+            _currentSource.Play();
 
-			_nextSource.Stop();
-			_nextSource.clip = null;
-		}
+            _nextSource.Stop();
+            _nextSource.clip = null;
+        }
 
-		public void Update()
-		{
-			if (_currentSource.isPlaying) { _currentSource.volume = VolumeFromProgress(_currentSource); }
-				
-			if (_nextSource.isPlaying) { _nextSource.volume = VolumeFromProgress(_nextSource); }
+        public void Update()
+        {
+            // Adjust volume of the current and next sources based on progress in the audio clips.
+            if (_currentSource.isPlaying) { _currentSource.volume = VolumeFromProgress(_currentSource); }
 
-			if (!_nextSource.isPlaying && !_choosenNextClip)
-			{
-				_nextSource.clip = GetNextClip(_currentSource.clip);
-				_choosenNextClip = true;
-			}
+            if (_nextSource.isPlaying) { _nextSource.volume = VolumeFromProgress(_nextSource); }
 
-			if (!_currentSource.isPlaying) { SwapSources(); }
+            // If the next source is not playing and the next clip hasn't been chosen, pick a new one.
+            if (!_nextSource.isPlaying && !_choosenNextClip)
+            {
+                _nextSource.clip = GetNextClip(_currentSource.clip);
+                _choosenNextClip = true;
+            }
 
-			if (GetProgress(_currentSource) > lastMarker && !_currentHasReachedDeclining)
-			{
-				_currentHasReachedDeclining = true;
-				_nextSource.Play();
-			}
-		}
+            // Swap sources when the current source has finished playing.
+            if (!_currentSource.isPlaying) { SwapSources(); }
+
+            // Start playing the next source when the current source has reached the lastMarker.
+            if (GetProgress(_currentSource) > lastMarker && !_currentHasReachedDeclining)
+            {
+                _currentHasReachedDeclining = true;
+                _nextSource.Play();
+            }
+        }
 
         #endregion
 
         #region Private Methods
 
+        /// <summary>
+        /// Start playing the next source.
+        /// </summary>
         private void PlayNext()
-		{
-			_nextSource.Play();
-		}
+        {
+            _nextSource.Play();
+        }
 
-		private void SwapSources()
-		{
-			AudioSource temp = _currentSource;
-			_currentSource = _nextSource;
-			_nextSource = temp;
+        /// <summary>
+        /// Swap the current and next audio sources.
+        /// </summary>
+        private void SwapSources()
+        {
+            AudioSource temp = _currentSource;
+            _currentSource = _nextSource;
+            _nextSource = temp;
 
-			_choosenNextClip = false;
-			_currentHasReachedDeclining = false;
-		}
+            _choosenNextClip = false;
+            _currentHasReachedDeclining = false;
+        }
 
-		private float VolumeFromProgress(AudioSource source)
-		{
-			float progress = GetProgress(source);
+        /// <summary>
+        /// Calculate the volume based on the progress in the audio clip.
+        /// </summary>
+        private float VolumeFromProgress(AudioSource source)
+        {
+            float progress = GetProgress(source);
 
-			if (progress < firstMarker)
-			{
-				return progress / firstMarker;
-			}
-			else if (progress > lastMarker)
-			{
-				return (1.0f - progress) / firstMarker;
-			}
-			else
-			{
-				return 1.0f;
-			}
-		}
+            if (progress < firstMarker) { return progress / firstMarker; }
+            else if (progress > lastMarker) { return (1.0f - progress) / firstMarker; }
+            else { return 1.0f; }
+        }
 
-		private float GetProgress(AudioSource source)
-		{
-			if (source != null && source.clip != null)
-				return (float)source.timeSamples / (float)source.clip.samples;
-			else
-				return 1.0f;
-		}
+        /// <summary>
+        /// Calculate the progress of the current audio clip.
+        /// </summary>
+        private float GetProgress(AudioSource source)
+        {
+            if (source != null && source.clip != null) { return (float)source.timeSamples / (float)source.clip.samples; }
+            else { return 1.0f; }
+        }
 
-		private AudioClip GetNextClip(AudioClip current)
-		{
-			if (clips.Count <= 1)
-				return null;
+        /// <summary>
+        /// Get the next audio clip, excluding the current clip if available.
+        /// </summary>
+        private AudioClip GetNextClip(AudioClip current)
+        {
+            if (clips.Count <= 1) { return null; }
 
-			AudioClip clip = clips.PickRandom();
-			while (clip == current && clip != null)
-			{
-				clip = clips.PickRandom();
-			}
+            AudioClip clip = clips.PickRandom();
+            while (clip == current && clip != null) { clip = clips.PickRandom(); }
 
-			return clip;
-		}
+            return clip;
+        }
 
         #endregion
     }
